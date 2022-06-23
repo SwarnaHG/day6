5609934a12bae132be305c6667076efde4ee91ae
SwarnaHG <swarnahg18@gmail.com>
2022-06-23 21:57:17 +0530

    Day 6 assignments

---
+++ src/Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the range");
+        int n=sc.nextInt();
+        System.out.println("Fibonacci Series till " + n + " terms:");
+        int firstTerm=0,secondTerm=1;
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + ", ");
+
+            // compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
